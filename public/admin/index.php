bb71a2e296e2cfc9a39ff789f7ca5ceca8af76f8
japan <49401941+japannt@users.noreply.github.com>
2023-05-11 08:34:17 +0200

    remove todo

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,16 +1,13 @@
 <?php
 require_once("../../header.php");
 
-// simple auth with username and password from config.php
-
 $username = $config["admin_username"];
 $password = $config["admin_password"];
 
 if(!isset($_SERVER["PHP_AUTH_USER"]) || !isset($_SERVER["PHP_AUTH_PW"]) || $_SERVER["PHP_AUTH_USER"] != $username || $_SERVER["PHP_AUTH_PW"] != $password) {
 	header("WWW-Authenticate: Basic realm=\"plog_admin\"");
 	header("HTTP/1.0 401 Unauthorized");
-	print("You must enter a valid username and password to access this page.");
-	exit();
+	die("You must enter a valid username and password to access this page.");
 }
 
 print($twig->render('admin.twig', []));

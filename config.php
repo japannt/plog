afc34386182060934681ea6171a4e5603ea3571e
japan <49401941+japannt@users.noreply.github.com>
2023-05-11 08:27:00 +0200

    minor cleanup

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 $config = [
 	"blog_name" => "My Blog",
+	"blog_description" => "A simple blog",
 	"blog_stylesheet" => "style.css",
 	"admin_username" => "admin",
 	"admin_password" => "change_me",

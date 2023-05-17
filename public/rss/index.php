b63d9979bce4e7ea9aeb61c7672829ea1769920b
japannt <49401941+japannt@users.noreply.github.com>
2023-05-17 13:25:26 +0200

    uqe

--- public/rss/index.php
+++ public/rss/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once("../../header.php");
 
-$posts = $db->query("SELECT * FROM posts ORDER BY id DESC LIMIT 10");
+$posts = $db->query("SELECT `id`, `title`, `description` FROM posts ORDER BY id DESC LIMIT 10");
 $posts->setFetchMode(PDO::FETCH_ASSOC);
 
 header("Content-Type: application/rss+xml; charset=utf-8");
